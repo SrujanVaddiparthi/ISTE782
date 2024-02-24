987aad69242eaff91cd449ac66b995ce0551088d
SrujanVaddiparthi <sv6126@g.rit.edu>
2024-02-23 21:35:20 -0500

    commit the map with new green colored numbers for the selected wards

--- ISTE782_VisualAnalytics/Chicago_HeatMap/crime_concentration_map.py
+++ ISTE782_VisualAnalytics/Chicago_HeatMap/crime_concentration_map.py
@@ -10,27 +10,28 @@ crime_data = pd.read_excel("/Users/wangtiles/ISTE782/ISTE782_VisualAnalytics/Chi
 geojson_file = "/Users/wangtiles/ISTE782/ISTE782_VisualAnalytics/Chicago_HeatMap/ward_boundaries.geojson"
 geo_data = gpd.read_file(geojson_file)
 
-# Checking for datatypes of 'ward' column in geo_data and 'Ward' column in crime_data
-print("Data type of 'ward' column in geo_data:", geo_data['ward'].dtype)
-print("Data type of 'Ward' column in crime_data:", crime_data['Ward'].dtype)
-
-# Convert 'ward' column in geo_data to string
-geo_data['ward'] = geo_data['ward'].astype('str')
-
-# Convert 'Ward' column in crime_data to string
-crime_data['Ward'] = crime_data['Ward'].astype('str')
-
-# Convert 'ward' column in geo_data to int64
+# Step 4: Convert 'ward' column in geo_data to int64
 geo_data['ward'] = geo_data['ward'].astype('int64')
 
-# Convert 'Ward' column in crime_data to int64
+# Step 5: Convert 'Ward' column in crime_data to int64
 crime_data['Ward'] = crime_data['Ward'].astype('int64')
 
-# Step 4: Merge crime data with GeoDataFrame
+# Step 6: Merge crime data with GeoDataFrame
 merged_data = geo_data.merge(crime_data, how='left', left_on='ward', right_on='Ward')
 
-# Step 5: Visualize the heatmap
+# Step 7: Visualize the heatmap
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 merged_data.plot(column='Crime_Concentration', cmap='Reds', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+
+# Defining the ward numbers selected with special highlighting
+highlight_ward_numbers = [3, 46, 1, 25, 34]
+
+# Add ward numbers to the center of each ward
+for idx, row in merged_data.iterrows():
+    ward_number = row['ward']
+    centroid = row['geometry'].centroid
+    color = 'green' if ward_number in highlight_ward_numbers else 'black'
+    ax.text(centroid.x, centroid.y, str(ward_number), fontsize=8, ha='center', va='center', color=color)
+
 plt.title('Crime Concentration Heatmap by Ward')
 plt.show()
